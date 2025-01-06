2a8a65232c2a4edb431bca21479edc6d8af39320
Ivan Nazarov <nazarovv676@gmail.com>
2025-01-06 11:06:18 +0200

    docs: enhance exception classes with documentation and final modifiers

--- lib/src/exceptions.dart
+++ lib/src/exceptions.dart
@@ -1,11 +1,15 @@
-class ReactiveDataException implements Exception {
+/// Base class for all exceptions thrown by the reactive data package
+abstract base class ReactiveDataException implements Exception {
   const ReactiveDataException();
 }
 
-class OptimisticUpdateException extends ReactiveDataException {
+/// Exception thrown when a update fails and the data is rolled back
+final class OptimisticUpdateException extends ReactiveDataException {
   const OptimisticUpdateException();
 }
 
-class DataFilteredException extends ReactiveDataException {
+/// Exception thrown when a first fetched data filter is applied and nothing
+/// could be returned in the data stream
+final class DataFilteredException extends ReactiveDataException {
   const DataFilteredException();
 }
